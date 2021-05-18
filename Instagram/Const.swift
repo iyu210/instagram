53eecc381e36c6c676e6df7f67b71724024edf06
iyu.swiftLeaner <iyu@live.jp>
2021-05-18 15:14:31 +0900

    instagramクローン作成

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 岩渕優児 on 2021/05/18.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
